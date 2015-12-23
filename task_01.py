5f45559e0e2578d08caf7dfea69ca5df333abe0d
emad8878 <emad5223@gmail.com>
2015-12-23 02:40:01 +0000

    modified task

--- task_01.py
+++ task_01.py
@@ -9,19 +9,49 @@ ABSOLUTE_DIFFERENCE = Decimal('273.15')
 
 
 def fahrenheit_to_celsius(degrees):
-    """ This function will convert fahrenheit to celsius."""
+    """ This function will convert fahrenheit to celsius.
+
+    Args:
+        degrees (mixed): temperature measurement
+        fc (decimal): value
+    Returns:
+        temperature in celcius
+    Example:
+        >>>fahrenhiet_to_celsius(30.0)
+        -1.111
+    """
 
     return (Decimal(degrees) -32)*5/9
 
 
 def celsius_to_kelvin(degrees):
-    """ This function convert celsius to kelvin."""
+    """ This function convert celsius to kelvin.
+
+    Args:
+        degrees (mixed): temperature value
+        ck (decimal): value
+    returns:
+        temperature in kelvin
+    Example:
+        >>>celsius_to_kelvin(10)
+        283.15
+    """
 
     return Decimal(degrees)+ ABSOLUTE_DIFFERENCE
 
 
 def fahrenheit_to_kelvin(degrees):
-    """This function convert fahrenheit to kelvin."""
+    """This function convert fahrenheit to kelvin.
+
+    Args:
+       degrees (mixes): temperature measurement
+       fk (decimal): values
+    returns:
+       temperature in kelvin
+    Examples:
+        >>> fahrenhiet_to_kelvin(30.2)
+        272.26
+    """
 
     # celsius_to_kelvin(fahrenheit_to_celsius())
     celsius = fahrenheit_to_celsius(degrees)
